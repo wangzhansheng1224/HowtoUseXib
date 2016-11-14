be934472e5f68ff12cccd6856f7b11e38ff6bbf2
王战胜 <wangzs@smartdot.com>
2016-11-14 17:54:10 +0800

    Initial Commit

---
+++ HowToUsexib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HowToUsexib
+//
+//  Created by 王战胜 on 2016/11/14.
+//  Copyright © 2016年 王战胜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HowToUsexib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HowToUsexib
+//
+//  Created by 王战胜 on 2016/11/14.
+//  Copyright © 2016年 王战胜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
